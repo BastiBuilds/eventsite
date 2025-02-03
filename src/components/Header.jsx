c823f66f7de8ee048e0993040f49dc46fb405c46
BastiBuilds <bastian.bueeler@gmail.com>
2025-02-03 19:54:41 +0100

    added Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h1>Welcome to EventFinder</h1>
+                <p>Discover exciting events in your region</p>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
