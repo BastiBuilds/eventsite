ae3119625e8b26e7d3a090fbd85f858678cdaec1
BastiBuilds <bastian.bueeler@gmail.com>
2025-02-03 19:55:32 +0100

    added Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <p>&copy; 2025 EventFinder. All rights reserved.</p>
+                <div>
+                    <a href="/impressum">Impressum</a> | <a href="/contact">Kontaktformular</a>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
