14f54b3d65aafe10c83af1fd6d1f69926ae03371
BastiBuilds <bastian.bueeler@gmail.com>
2025-02-04 01:05:10 +0100

    navbar changes but mobile version is bugging at the moment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
-import 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../index.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navbar">
             <h1>EventFinder</h1>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/search">Search</Link>
-                <Link to="/events">Events</Link>
-            </div>
+            <button className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+            {/* Desktop-Navbar */}
+            <ul className="navbar-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/search">Search</Link></li>
+                <li><Link to="/events">Events</Link></li>
+            </ul>
+            {/* Mobile-Navbar */}
+            <ul className={`navbar-links-mobile ${isOpen ? 'open' : ''}`}>
+                <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+                <li><Link to="/search" onClick={() => setIsOpen(false)}>Search</Link></li>
+                <li><Link to="/events" onClick={() => setIsOpen(false)}>Events</Link></li>
+            </ul>
         </nav>
     );
 };
