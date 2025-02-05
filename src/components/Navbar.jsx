5a2047dc45921d062bbbd7f0dcf01dfc621e920c
BastiBuilds <bastian.bueeler@gmail.com>
2025-02-06 00:54:44 +0100

    created new css for Navbar and removed sticky bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../index.css';
+import './styles/Navbar.css';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
